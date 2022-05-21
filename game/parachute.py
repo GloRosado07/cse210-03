01a29c0c7a16ac35d8707f6391b7fb1d8c75e231
Joseph <per16068@byui.edu>
2022-05-20 22:59:38 -0400

    added parachute class with number of attemps

--- game/parachute.py
+++ game/parachute.py
@@ -1,2 +1,31 @@
+from collections import deque
+from word import Word
 class parachute:
-    pass
+    def __init__(self):
+        self.parachute=deque([" ___","/___\\","\\   /"," \\ /","  O"," /|\\"," / \\"])
+        self.attemps = 0
+        self.words = Word()
+    #Will render the parachute
+    def render(self):
+        for i in self.parachute:
+            print(f" {i}")
+        print("\n^^^^^^^")
+    #Cut the parachute by parts
+    def cut_parachute(self):
+        self.attemps = len(self.parachute) - 3  
+        self.parachute.popleft()
+        if self.attemps - 3 <= 0:
+            self.parachute[0] = "  x"
+
+
+
+para = parachute()
+para.render()
+para.cut_parachute()
+para.render()
+para.cut_parachute()
+para.render()
+para.cut_parachute()
+para.render()
+para.cut_parachute()
+para.render()
