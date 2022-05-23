4fcf0bc5eb745e75b099434823e893536c8fe541
Jonathaup <103954802+Jonathaup@users.noreply.github.com>
2022-05-23 14:03:59 -0600

    Homework

--- game/driver.py
+++ game/driver.py
@@ -1,9 +1,63 @@
 from parachute import parachute
 from word import Word
+# from player import player
 
 class Driver:
-    def __init__(self):
-        pass
+    '''
+    The Driver class will start the game. 
+    
+    '''
+    def __init__(self) -> None:
+        '''
+        Atributes: 
+        _word > to pick random word from a list of words. 
+        _play_game > It will be used in a while loop to keep playing the game
+        _player > It will ask the user for a letter. 
+        _parachute > it create a figure of the man with a parachute. 
+        
+        '''
+        self._word = Word()
+        self._play_game = True
+        # self._player = Player()
+        self._parachute = parachute()
 
+    def start_game(self):
+        '''
+        Starts the game
+        
+        '''
+
+        while self._play_game:
+            self._do_output()
+            self._get_input()
+            self._do_update() 
+
+    def _do_output(self):
+        '''
+        Ask the user for a letter
+        Display the figure of the man with a parachute.
+        '''
+        self._word._get_word()
+        self._parachute._body(self._word._get_wrong_guesses())
+
+    def _get_input(self):
+        ask_player = input("\nGuess a letter [a-z]: ")
+        self._player.set_input(ask_player)
+
+    def _do_update(self):
+
+        self._word._check_word(self._player.get_input())
+        self._parachute._body(self._word._get_wrong_guesses())
+        if self._word._check_win() == 1:
+        
+            '''
+            It will keep asking the user for an input letter and display answer. 
+            '''
+            
+            self._word._check_word(self._player.get_input())
+            self._parachute._body(self._word._get_wrong_guesses())
+            
+        if self._word._check_win() == 1 or self._word._get_wrong_guesses() == 7:
+            self._play_game = False
 
 
